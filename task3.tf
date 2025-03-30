1a094f391790b1afdf3d611554bad4443f310cf6
johanwij <131279066+johanwij@users.noreply.github.com>
2025-03-30 22:50:30 +0200

    Update task3.tf

--- task3.tf
+++ task3.tf
@@ -1,1 +1,12 @@
+data "google_compute_subnetwork" "dev_mgmt_subnet" {
+  name    = "griffin-dev-mgmt"
+  region  = var.region             
+  project = var.project_id         
+}
+
+data "google_compute_subnetwork" "prod_mgmt_subnet" {
+  name    = "griffin-prod-mgmt"
+  region  = var.region             
+  project = var.project_id         
+}
 
