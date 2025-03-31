0a8717916cf4255aa2c0668641c296e734e7cbb5
johanwij <131279066+johanwij@users.noreply.github.com>
2025-03-31 11:16:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "project_id" {
   description = "The ID of the Google Cloud project"
   type        = string
-  default     = "qwiklabs-gcp-04-c9bba69bfd76" # Replace with your project ID
+  default     = "qwiklabs-gcp-02-6ba5e5a23798" # Replace with your project ID
 }
 
 variable "region" {
   description = "The Google Cloud region"
   type        = string
-  default     = "us-central1" # Replace with your desired region
+  default     = "us-west1" # Replace with your desired region
 }
 
 variable "zone" {
   description = "The Google Cloud ZONE"
   type        = string
-  default     = "us-central1-f" # Replace with your desired zone
+  default     = "us-west1-a" # Replace with your desired zone
 }
 
 variable "vpc_name_dev" {
